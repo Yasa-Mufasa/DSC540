3183c80f563c0a0d36b15f8fb15b428c3a92b5d1
YasaMufasa <43527639+YasaMufasa@users.noreply.github.com>
2019-01-26 13:45:42 -0700

    Adding Descriptions for Parts I, II, and III

--- Week7/exercise_7_2.py
+++ Week7/exercise_7_2.py
@@ -5,9 +5,42 @@ Joshua Gardner
 '''
 
 '''
+Part I:
 Importing Data - (Data Wrangling with Python, Page 219 - 228)
 Create a function to take an empty list, interate over the columns and create a full list of all the column types for
 the dataset. Then load into agate table - make sure to clean the data if you get an error. Follow along with the example
 in the book on the pages listed.
+'''
+
+
+
+
+
+
+
+
+'''
+Part II:
+Exploring Table Functions - (Data Wrangling with Python, Page 225 - 228)
+* Which countries have the highest rate of child labor?
+* Which countries have the most girls working?
+* What is the average percentage of child labor in cities?
+* Find a  row with more than 50% of rural child labor.
+* Rank the worst offenders in terms of child labor percentages by country.
+* Calculate the percentage of children not involved in child labor.
+'''
+
+
+
+
+
+
+
+
+'''
+Part III:
+Charting with matplotlib - (Data Wrangling with Python, page 255 - 258)
+* Chart the perceived corruption scores compared to the child labor percentages.
+* Chart the perceived corruption scores compared to the child labor percentages using only the worst offenders.
 '''
 
