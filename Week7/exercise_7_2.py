00f3a4efef6e8aae5f9660f3165328750e090dee
YasaMufasa <43527639+YasaMufasa@users.noreply.github.com>
2019-01-26 13:42:07 -0700

    Adding Exercise 7.2 to DSC540

---
+++ Week7/exercise_7_2.py
@@ -0,0 +1,13 @@
+'''
+DCS 540
+Exercise 7.2: Importing, Exploring, and Charting Data
+Joshua Gardner
+'''
+
+'''
+Importing Data - (Data Wrangling with Python, Page 219 - 228)
+Create a function to take an empty list, interate over the columns and create a full list of all the column types for
+the dataset. Then load into agate table - make sure to clean the data if you get an error. Follow along with the example
+in the book on the pages listed.
+'''
+
