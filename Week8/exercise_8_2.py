f3a505030fb75046dd711e9f2227b894d1f0076f
YasaMufasa <43527639+YasaMufasa@users.noreply.github.com>
2019-02-02 16:59:47 -0700

    Adding Part 2 and the beginning of Part 3

--- Week8/exercise_8_2.py
+++ Week8/exercise_8_2.py
@@ -20,6 +20,9 @@ notebook or code editor, no need to export your work.
 import urllib
 import urllib2
 import requests
+from bs4 import BeautifulSoup
+from lxml import html
+from selenium import webdriver
 
 '''
 Alright, first thing first, I need to import the various packages I'll need for this assignment.
@@ -78,7 +81,124 @@ That's cool, and helps give more information than I was thinking it would. And t
 assignment.
 
 Now to move onto Part 2:
-Reading a Web Page with Beautiful Soup - following the example starton page 300 - 304 of Data Wrangling with Python, use
+Reading a Web Page with Beautiful Soup - following the example starting on page 300 - 304 of Data Wrangling with Python, use
 the Beautiful Soup Python library to scrap a web page. The result should be data and output in an organized format. Each
 of the data entries should be in its own dictionary with matching keys.
 '''
+
+page = requests.get('http://www.enoughproject.org/take_action')
+bs = BeautifulSoup(page.content, features="html.parser")
+# print('Title:')
+# print(bs.title)
+# print('')
+# print("All a's:")
+# print(bs.find_all('a'))
+# print('')
+# print("All p's:")
+# print(bs.find_all('p'))
+# TODO: Remove comments before turning in.
+
+'''
+That's a lot but shows that you can grab what you need with the right requests.
+
+Now let's look at the family relationships in the HTML
+'''
+
+header_children = [c for c in bs.head.children]
+# print('Header Children')
+# print(header_children)
+
+navigation_bar = bs.find(id="globalNavigation")
+
+# for d in navigation_bar.descendants:
+#     print(d)
+#
+# for s in d.prevous_siblings:
+#     print(s)
+
+'''
+Looks like these won't work because there is nothing in navigation_bar. Looks like the website has been updated since
+our textbook was published and all of the "globalNavigation" have been removed. I double checked the website as well.
+Searching for "global" finds 1 location, and searching for "navigation" finds 6, but no instances of "globalNavigation".
+'''
+
+page = requests.get('http://www.enoughproject.org/take_action')
+bs = BeautifulSoup(page.content, "lxml")
+ta_divs = bs.find_all("div")    # removed the "class_='views-row'" option. This caused it to work.
+# print(len(ta_divs))
+# for ta in ta_divs:
+#     title = ta.h2
+#     link = ta.a
+#     about = ta.find_all('p')
+#     print(title, link, about)
+# TODO: Remove the comment before turning in.
+
+'''
+Hmmmm, I'm getting that there are no ta_divs. Let's check the website to see how many there are.
+Huh, I see that there are 124 "div" within the HTML. So why didn't it find them all?
+
+By removing the "class_='views-row'", I was now able to find 100 instances of this.
+'''
+
+all_data = []
+# for ta in ta_divs:
+#     data_dict = {}
+#     data_dict['title'] = ta.h2.get_text()
+#     data_dict['link'] = ta.a.get('href')
+#     data_dict['about'] = [p.get_text() for p in ta.find_all('p')]
+#     all_data.append(data_dict)
+#
+# print(all_data)
+# Commenting out to try completing the same thing using the lxml package
+'''
+Looks like the website has been updated even more. I'm running into the same issue as before, but in this case, there are
+no h2 attributes. But that completes this section of the textbook. Before moving on, though, I want to try to complete
+this section, but by using the lxml package.
+'''
+
+# page = html.parse('http://enoughproject.org/take_action')
+# root = page.getroot()
+# ta_divs = root.cssselect('div.views-row')
+# all_data = []
+# for ta in ta_divs:
+#     data_dict = {}
+#     title = ta.cssselect('h2')[0]
+#     data_dict['title'] = title.text_content()
+#     data_dict['link'] = title.find('a').get('href')
+#     data_dict['about'] = [p.text_content() for p in ta.cssselect('p')]
+#     all_data.append(data_dict)
+# print(all_data)
+
+'''
+Looks like I'm running into another issue. The parser isn't able to find the website. Well, that's irritating. I wonder
+if it would work using a different website? But which one? Why not Bellevue's webiste again?
+'''
+
+# page = html.parse('http://www.bellevue.edu')
+# root = page.getroot()
+# ta_divs = root.cssselect('div.views-row')
+# all_data = []
+# for ta in ta_divs:
+#     data_dict = {}
+#     title = ta.cssselect('h2')[0]
+#     data_dict['title'] = title.text_content()
+#     data_dict['link'] = title.find('a').get('href')
+#     data_dict['about'] = [p.text_content() for p in ta.cssselect('p')]
+#     all_data.append(data_dict)
+# print(all_data)
+# TODO: Remove comments before turning in.
+
+'''
+Well, it sort of works. This produces an empty list of dictionaries, unfortunately. This could be because this script is
+not personalized to Bellevue's website. Either that, or the 'views-row' command isn't doing what I'm expecting it to.
+Anyway, this completes this section
+
+Now it's time to move onto Part 3:
+Web scraping with Selenium - Follow along with your book starting on page 318 - 329 of Data Wrangling with Python. At
+the end of the exercise, you should be able to go to a site, fill out a form, submit the form, and then scroll through
+the results with the code you wrote. Make sure to submit teh code and your output.
+'''
+
+browser = webdriver.Firefox()
+browser.get('http://www.fairphone.com/we-are-fairphone/')
+browser.maximize_window()
