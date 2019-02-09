0ab6860b1dd468c7046843457b21c88f5092400d
YasaMufasa <43527639+YasaMufasa@users.noreply.github.com>
2019-02-09 11:13:37 -0700

    Adding exercise_9_2 along with explanation of the exercise

---
+++ Week9/exercise_9_2.py
@@ -0,0 +1,61 @@
+'''
+DSC 540
+Exercise 9.2: Pulling Data from APIs
+Joshua Gardner
+'''
+
+'''
+Complete the following using Python - make sure to show your work and show the values returned. YOu can submit via your
+notebook or code editor, no need to export your work.
+
+In this exercise, you will create a twitter account (if you don't already have one, or don't wish to use your personal
+account) and practice pulling data from Twitter's publically available API. You can delete the twitter account as soon
+as you have completed the exercise. Include your code and output for each step.
+    1. Create a Twitter API Key and Access Token (Data Wrangling with Python, pg 365 - 366)
+    2. Do a single data pull from Twitter's REST API (Data Wrangling with Python, pg 366 - 368)
+    3. Execute multiple queries at a time from Twitter's REST API (Data Wrangling with Python, pg 368 - 371)
+    4. Do a data pull from Twitter's Streaming API (Data Wrangling with Python, pg 372 - 374)
+'''
+
+'''
+Part 1 - Create a Twitter API Key and Access Token (Data Wrangling with Python, pg 365 - 366)
+
+The first step to working on this is that you need to have an account with Twitter. I don't want to use my personal
+Twitter account for this yet, so I'm creating an account at https://twitter.com/signup.
+'''
+
+
+
+
+
+
+
+'''
+Part 2 - Do a single data pull from Twitter's REST API (Data Wrangling with Python, pg 366 - 368)
+
+
+'''
+
+
+
+
+
+
+
+
+
+'''
+Part 3 - Execute multiple queries at a time from Twitter's REST API (Data Wrangling with Python, pg 368 - 371)
+'''
+
+
+
+
+
+
+
+
+
+'''
+Part 4 - Do a data pull from Twitter's Streaming API (Data Wrangling with Python, pg 372 - 374)
+'''
