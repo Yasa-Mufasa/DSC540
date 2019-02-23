f46155d5a1b3b647e1664e881497cc99d5f8ed3b
YasaMufasa <43527639+YasaMufasa@users.noreply.github.com>
2019-02-23 10:12:12 -0700

    Adding Week 11 exercise and description

---
+++ Week11/exercise_11_2.py
@@ -0,0 +1,16 @@
+'''
+DSC 540 Data Preparation
+Exercise 11.2: Data Wrangling Tools
+Joshua Gardner
+'''
+
+'''
+Exercise 11.2:
+Complete the following using Python - make sure to show your work and show the values returned. You can submit via your
+notebook or code editor, no need to export your work.
+
+Going back to Chapter 9 (Data Wrangling with Python), let's practice joining numerous data sets - an activity you will
+likely run into frequently. Following hte example in your text that starts on page 229 - 233 of Data Wrangling with
+Python, work through the example to bring two data sets together. Submit your code and output to the assignment link.
+'''
+
