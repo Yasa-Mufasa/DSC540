aba45a56ab6769d1570299851f7676f26812cb62
YasaMufasa <43527639+YasaMufasa@users.noreply.github.com>
2019-02-23 14:32:30 -0700

    Adding the second table and joining the two tables together

--- Week11/exercise_11_2.py
+++ Week11/exercise_11_2.py
@@ -69,6 +69,16 @@ def get_new_array(old_array, function_to_clean):
     return new_arr
 
 
+def remove_bad_chars(val):
+    if val == '-':
+        return None
+    return val
+
+
+def reverse_percent(row):
+    return 100 - row['Total (%)']
+
+
 cpi_workbook = xlrd.open_workbook('Data/corruption_perception_index.xls')
 cpi_sheet = cpi_workbook.sheets()[0]
 
@@ -109,3 +119,90 @@ Ah, I need the child labor table from earlier in chapter 9. Alright, let's go an
 correctly. Then I can get do the join function.
 '''
 
+workbook = xlrd.open_workbook('Data/unicef_oct_2014.xls')
+sheet = workbook.sheets()[0]
+title_rows = zip(sheet.row_values(4), sheet.row_values(5))
+titles = [t[0] + ' ' + t[1] for t in title_rows]
+titles = [t.strip() for t in titles]    # Getting the titles into the correct format
+
+country_rows = [sheet.row_values(r) for r in range(6, 114)] # Removes the title rows, just has the data
+
+# Now I need the data types. I should be able to reuse the different types that were defined above.
+
+example_row = sheet.row(6)
+
+types = []
+
+for v in example_row:
+    value_type = ctype_text[v.ctype]
+    if value_type == 'text':
+        types.append(text_type)
+    elif value_type == 'number':
+        types.append(number_type)
+    elif value_type == 'xldate':
+        types.append(date_type)
+    else:
+        types.append(text_type)
+
+
+'''
+Need to clean the rows before creating the table.
+'''
+
+
+cleaned_rows = []
+
+for row in country_rows:
+    cleaned_row = [remove_bad_chars(rv) for rv in row]
+    cleaned_rows.append(cleaned_row)
+
+cleaned_rows = get_new_array(country_rows, remove_bad_chars)
+
+table = agate.Table(cleaned_rows, titles, types)
+
+'''
+Alright, I've got the table, but let's work on it a little bit more before trying the join operation. Following along
+with the textbook, let's go ahead and try ordering the data in the table.
+'''
+
+most_egregious = table.order_by('Total (%)', reverse=True).limit(10)
+
+female_data = table.where(lambda r: r['Female'] is not None)
+most_females = female_data.order_by('Female', reverse=True).limit(10)
+
+table.aggregate(agate.Mean('Place of residence (%) Urban'))
+has_por = table.where(lambda r: r['Place of residence (%) Urban'] is not None)
+has_por.aggregate(agate.Mean('Place of residence (%) Urban'))
+first_match = has_por.find(lambda x: x['Rural'] > 50)
+
+'''
+Now to get the second table that we're interested in for the join.
+'''
+
+ranked = table.compute([('Total Child Labor Rank',
+                         agate.Rank('Total (%)', reverse=True)), ])
+
+ranked = table.compute([('Children not working (%)',
+                         agate.Formula(number_type, reverse_percent)),
+                        ])
+
+ranked = ranked.compute([('Total Child Labor Rank',
+                          agate.Rank('Children not working (%)')),
+                         ])
+
+'''
+Alright, I have the two tables to do the join and they're in the format I want them to be in. Now I can run the join
+function. Let's do an inner join.
+'''
+
+cpi_and_cl = cpi_table.join(ranked, 'Country / Territory', 'Countries and areas', inner=True)
+
+for r in cpi_and_cl.order_by('CPI 2013 Score').limit(10).rows:
+    print('{}: {} - {}%'.format(r['Country / Territory'],
+                                r['CPI 2013 Score'],
+                                r['Total (%)']
+                                ))
+
+'''
+And that does it. We've joined the two tables together and printed out values from joined table.
+'''
